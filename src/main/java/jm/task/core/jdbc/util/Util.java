b1c94343ea43837cbcb0767affee666c4fbd1d51
Azamat244 <152506960+Azamat244@users.noreply.github.com>
2024-12-18 19:30:08 +0500

    Удалил неиспользуемые импорты. Поправил инкапсуляцию. Добавил метод closeConnection. Добавил откат через rollback для DML операций. Добавил аннотацию @Override

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,24 +3,46 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/database";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/db";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "2443254";
+
+    private static Logger logger = Logger.getLogger(Util.class.getName());
+
+    private Util() {
+
+    }
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            connection.setAutoCommit(false);
             System.out.println("Соединение с БД установлено");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
 
         }
         return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                logger.info("Соединение с БД закрыто");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                logger.info("Ошибка при закрытии соединения с БД");
+            }
+        } else {
+            logger.info("Соединение уже было закрыто или не инициализировано");
+        }
     }
 }
